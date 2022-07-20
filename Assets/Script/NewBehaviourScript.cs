63e8f4ee7b25dd4efd3cfb81a9077708b45c30ed
huic05 <huitungc1228@gmail.com>
2022-07-20 15:57:58 -0400

    7/20

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -7,17 +7,37 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     private BoxCollider myCollider;
     private Transform myTransform;
+    private Camera mycamera;
+    private Transform otherPlace;
 
 
     void Start()
     {
         myCollider = GetComponent<BoxCollider>();
-        myTransform = GetComponent<Transform>();    
+        myTransform = GetComponent<Transform>();
+        print(myCollider.name);
+        mycamera = transform.GetChild(0).GetComponent<Camera>();
+        otherPlace = GameObject.Find("Cube").GetComponent<Transform>();
+
     }
 
     void Update()
     {
-        transform.position += new Vector3(0.01f, 0.01f, 0.01f);
+        float x = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            x = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            x = -1;
+        }
+        else
+        {
+            x = 0;
+        }
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vetical"));
+        transform.position = movement;
     }
 
 }
