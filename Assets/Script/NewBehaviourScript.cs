7763bf7ca023767b0c2301d199bd9aceb8ff9ed9
huic05 <huitungc1228@gmail.com>
2022-07-21 15:02:21 -0400

    7/21

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    
+    public float x = 1;
+    public float z = 1;
+    private CharacterController characterController;
+    private Camera _camera;
+    /*
     // Start is called before the first frame update
     private BoxCollider myCollider;
     private Transform myTransform;
     private Camera mycamera;
     private Transform otherPlace;
-
+    */
 
     void Start()
     {
+        /*
         myCollider = GetComponent<BoxCollider>();
         myTransform = GetComponent<Transform>();
         print(myCollider.name);
         mycamera = transform.GetChild(0).GetComponent<Camera>();
-        otherPlace = GameObject.Find("Cube").GetComponent<Transform>();
-
+        otherPlace = GameObject.Find("Cube").GetComponent<Transform>();*/
+        characterController = GetComponent<CharacterController>();
+        _camera = transform.GetChild(0).GetComponent<Camera>();
     }
 
     void Update()
     {
-        float x = 0;
+        /*float x = 0;
         if (Input.GetKey(KeyCode.W))
         {
             x = 1;
@@ -35,9 +43,22 @@ public class NewBehaviourScript : MonoBehaviour
         else
         {
             x = 0;
+        } */
+    float time = Time.deltaTime;
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal") * time , 0, Input.GetAxis("Vertical") * time);
+        characterController.Move(move);
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+        _camera.transform.Rotate(Input.GetAxis("Mouse Y"), 0, 0);
+        print(_camera.transform.eulerAngles);
+        if (_camera.transform.eulerAngles.x >=50)
+        {
+            _camera.transform.eulerAngles = new Vector3(Mathf.Clamp(_camera.transform.eulerAngles.x - 360, -30, 0), _camera.transform.eulerAngles.y, 0);
+
         }
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vetical"));
-        transform.position = movement;
+        else
+        {
+            _camera.transform.eulerAngles = new Vector3(Mathf.Clamp(_camera.transform.eulerAngles.x, 0, 45), _camera.transform.eulerAngles.y, 0);
+        }
     }
 
 }
